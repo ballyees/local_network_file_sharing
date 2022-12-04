8c4bee0bfa1489b5e8ce7b07742e1345593f315e
ballyees <shouldertip@gmail.com>
2022-12-04 22:16:44 +0700

    fix | allowed all hosts

--- file_sharing/settings.py
+++ file_sharing/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -26,9 +25,7 @@ SECRET_KEY = 'django-insecure-pzqz6=ha^o$gm2hbuhg7#g-5hd@9$qnlt%co^j61n5^o*hc0-b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
